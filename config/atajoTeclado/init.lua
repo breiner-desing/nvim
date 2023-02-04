5050eab9abfe1b4bd5a29567f9a52e40addb90cc
breiner <breiner2552@gmail.com>
2023-02-04 14:51:43 -0500

    plugin lualine and nvim-tree

---
+++ config/atajoTeclado/init.lua
@@ -0,0 +1,5 @@
+local nmap = vim.keymap
+
+nmap.set('n','<C-s>', '<cmd>write<cr>', {desc = 'guardar'})
+nmap.set('n', '<C-b>', '<cmd>NvimTreeToggle<cr>', { desc = 'desplegar menu' } )
+
--- init.lua
+++ init.lua
@@ -1,5 +1,9 @@
 require("config/configBasic/init")
+require("config/atajoTeclado/init")
+require("config/plugin/init")
+require("config/icons/init")
+require("config/lualine/init")
 
 
-
+local x = 'Breiner Andres Rojas Angulo'
 
