f6ae693e81fc1fad43c228f9566c7e15566752ed
breiner <breiner2552@gmail.com>
2023-02-03 21:39:31 -0500

    funcionalidad basica

---
+++ init.lua
@@ -0,0 +1,16 @@
+local wo = vim.wo
+				
+wo.number = true
+wo.rnu = true
+
+vim.opt.scs = true
+
+vim.opt.mouse = 'a'
+
+vim.opt.ignorecase = true
+
+vim.opt.smartcase = true
+
+vim.opt.wrap = true
+
+vim.opt.tabstop = 2
