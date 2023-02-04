f1bf6e75b0a656e9ed5cccc0c2491a32972edd60
breiner <breiner2552@gmail.com>
2023-02-03 21:55:17 -0500

    separar configuracion

--- init.lua
+++ init.lua
@@ -1,16 +1,5 @@
-local wo = vim.wo
-				
-wo.number = true
-wo.rnu = true
+require("config/configBasic/init")
 
-vim.opt.scs = true
 
-vim.opt.mouse = 'a'
 
-vim.opt.ignorecase = true
 
-vim.opt.smartcase = true
-
-vim.opt.wrap = true
-
-vim.opt.tabstop = 2
