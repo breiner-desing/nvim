82dcf6a35a745581880a1b943a8995bf19bf4cf3
breiner <breiner2552@gmail.com>
2023-02-07 21:35:59 -0500

    c

--- init.lua
+++ init.lua
@@ -5,17 +5,22 @@ require("config/icons/init")
 require("config/lualine/init")
 require("config/telescope/init")
 
---require("config/git/init")
+--require("airblade").startup()
 
 vim.cmd[[colorscheme tokyonight]]
-
 
 --proximamente
 --require("config/treesitter/init.lua")
 --
 --implementation the COC 
-
-
-
-
 local x = 'Breiner Andres Rojas Angulo'
+
+
+--local configuracion = {
+--"java.configuration.runtimes": [
+--  {
+--    "name": "JavaSE-1.8",
+--    "path": "/path/to/jdk-8",
+--  }]
+--}
+
